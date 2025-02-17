eee602d73c017da6e150e401b44561275f9bc42b
admin <admin@MOHAMADNAIM>
2025-02-17 14:16:28 +0530

    Add project files.

---
+++ Migrations/20250214091650_AddGenderColum.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenderColum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Models/AddStudentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using WebApplication1.Migrations;
+
+namespace WebApplication1.Models
+{
+    public class AddStudentViewModel
+    {
+        public string?   Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public bool Subscribed { get; set; }
+
+        public string? BloodGroup { get; set; }
+
+        public string? Gender { get; set; }
+    }
+}
